bf1453a49a8a8ead30378c6f7cca885ff622b934
TheLittleCitizen9 <thelittlecitizen9@gmail.com>
2020-08-13 08:51:33 +0300

    Added linq functions

---
+++ LinqBasic/LinqBasic/PersonRetriver.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using LinqBasic.Abstract;
+
+namespace LinqBasic
+{
+    public class PersonRetriver : IPersonRetriver
+    {
+        public IEnumerable<int> GetMalesId(IEnumerable<Person> persons)
+        {
+            return persons.Where(p => p.PersonGender == Gender.Male).Select(p => p.Id);
+        }
+
+        public IEnumerable<Person> GetOddIds(IEnumerable<Person> persons)
+        {
+            return persons.Where(p => p.Id % 2 != 0);
+        }
+
+        public IEnumerable<Person> OrderById(IEnumerable<Person> persons)
+        {
+            return persons.OrderByDescending(p => p.Id);
+        }
+    }
+}
---
+++ LinqBasic/LinqBasic/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using LinqBasic.Abstract;
+
+namespace LinqBasic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<Person> persons = new List<Person>()
+                                       {
+                                           new Person()
+                                               {
+                                                   Id = 1,
+                                                   Name = "Tomer",
+                                                   PersonGender = Gender.Male
+                                               },
+                                           new Person()
+                                               {
+                                                   Id = 2,
+                                                   Name = "Tami",
+                                                   PersonGender = Gender.Female
+                                               },
+                                                                                              new Person()
+                                               {
+                                                   Id = 3,
+                                                   Name = "Nadav",
+                                                   PersonGender = Gender.Male
+                                               },
+                                                                                           new Person()
+                                               {
+                                                   Id = 4,
+                                                   Name = "Roy",
+                                                   PersonGender = Gender.Male
+                                               },
+                                                                                          new Person()
+                                               {
+                                                   Id = 5,
+                                                   Name = "Bar",
+                                                   PersonGender = Gender.Female
+                                               },
+
+                                       };
+            PersonRetriver personRetriver = new PersonRetriver();
+
+            var malesId = personRetriver.GetMalesId(persons);
+            foreach (var item in malesId)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("===================");
+            var orderById = personRetriver.OrderById(persons);
+            foreach (var item in orderById)
+            {
+                Console.WriteLine(item.Id);
+            }
+
+            Console.WriteLine("===================");
+            var oddIds = personRetriver.GetOddIds(persons);
+            foreach (var item in oddIds)
+            {
+                Console.WriteLine(item.Id);
+            }
+
+
+        }
+    }
+}
